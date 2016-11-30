d548e92fb19445303dc9235ff87e3c916179f574
gadflysu <gadflysu@users.noreply.github.com>
2016-11-30 23:55:59 +0800

    AC: hdoj_1001~1002

---
+++ HDOJ/hdoj_1001.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+long int sum(int n);
+
+int main()
+{
+	int n;
+	
+	while (scanf("%d", &n) != EOF) {
+		printf("%d\n\n", sum(n));
+	}
+	
+	return 0;
+}
+
+long int sum(int n)
+{
+	int i, s = 0;
+	
+	for (i = 1; i <= n; i++)
+		s += i;
+
+	return s;
+}
